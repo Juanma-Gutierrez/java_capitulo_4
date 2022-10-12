d5c6b36606c6199ddd99dc1da7d12adba8589b74
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-12 13:58:59 +0200

    feat: Ejercicio6

---
+++ Ejercicio6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Exercise: 6
+ *
+ * @version: 12/10/2022 13:52
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float h;
+        float res;
+        System.out.print("Introduce la altura: ");
+        h = sc.nextInt();
+        res = (float) Math.pow(2 * h / 9.81, 2);
+        System.out.print("El tiempo en caer desde una altura " + h + " es de "
+                + String.format("%.2f", res));
+        sc.close();
+    }
+}
