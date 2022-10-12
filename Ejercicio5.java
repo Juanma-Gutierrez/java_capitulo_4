0b6425fe62a8ecaac529363eb2a20d7ec8713c3f
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-12 13:09:31 +0200

    feat: Ejercicio5

---
+++ Ejercicio5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Exercise: 5
+ *
+ * @version: 12/10/2022 13:03
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a;
+        float b;
+        System.out.println("Este programa resuelve ecuaciones de primer grado del tipo ax + b = 0");
+        System.out.print("Por favor, introduzca el valor de a: ");
+        a = sc.nextInt();
+        System.out.print("Ahora introduzca el valor de b: ");
+        b = sc.nextInt();
+        System.out.print(a != 0 ? "x = " + (-b / a) : "Esa ecuación no tiene solución real");
+        sc.close();
+    }
+}
