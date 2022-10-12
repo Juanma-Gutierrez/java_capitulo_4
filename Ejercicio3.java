07ccce37e000c5a743e1b3208e9ea44f6553f2a0
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-12 12:43:59 +0200

    feat: Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * Exercise: 3
+ *
+ * @version: 12/10/2022 12:38
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        String msg;
+        System.out.print("Introduce un número (1-7): ");
+        num = sc.nextInt();
+        switch (num) {
+            case 1:
+                msg = "El día 1 de la semana es: Lunes";
+                break;
+            case 2:
+                msg = "El día 2 de la semana es: Martes";
+                break;
+            case 3:
+                msg = "El día 3 de la semana es: Miércoles";
+                break;
+            case 4:
+                msg = "El día 4 de la semana es: Jueves";
+                break;
+            case 5:
+                msg = "El día 5 de la semana es: Viernes";
+                break;
+            case 6:
+                msg = "El día 6 de la semana es: Sábado";
+                break;
+            case 7:
+                msg = "El día 7 de la semana es: Domingo";
+                break;
+            default:
+                msg = "Has introducido un número no válido";
+                break;
+        }
+        System.out.println(msg);
+        sc.close();
+    }
+}
