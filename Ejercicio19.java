74de045f401b779202fd93b2c2befca01b0877a9
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-16 09:45:09 +0200

    feat: Ejercicio19

--- Ejercicio19.java
+++ Ejercicio19.java
@@ -1,31 +1,29 @@
 import java.util.Scanner;
 
 /**
- * Exercise: 18
+ * Exercise: 19
  *
- * @version: 16/10/2022 09:13
+ * @version: 16/10/2022 09:41
  * @author: Juan Manuel Gutiérrez
  *          https://github.com/Juanma-Gutierrez
  */
 
-public class Ejercicio18 {
+public class Ejercicio19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num;
-        int res;
-        int newNum;
+        int cont;
         System.out.print("Introduce un número: ");
         num = sc.nextInt();
-        newNum = 0;
-
+        cont = 0;
+        if (num < 0)
+            num *= (-1);
         do {
-            res = num % 10;
-            newNum *= 10;
-            newNum += res;
             num /= 10;
+            cont++;
 
         } while (num > 0);
-        System.out.print(newNum);
+        System.out.print(cont);
         sc.close();
     }
 }
