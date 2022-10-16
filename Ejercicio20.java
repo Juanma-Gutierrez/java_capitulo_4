79046015c09c5e47bcc1fe064a45254f95f93116
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-16 11:03:33 +0200

    fix Ejercicio20

--- Ejercicio20.java
+++ Ejercicio20.java
@@ -17,18 +17,23 @@ public class Ejercicio20 {
         boolean cap;
         int neg;
         cap = true;
-        System.out.print("Introduce un número entero: ");
+        System.out.print("Introduce un número entero de hasta 5 dígitos de tamaño: ");
         num = sc.nextInt();
         longNum = Integer.toString(num).length();
         neg = (num < 0) ? 1 : 0;
-        for (int i = 0; i < longNum / 2; i++) {
-            if (Integer.toString(num).charAt(i + neg) != Integer.toString(num).charAt(longNum - i - 1)) {
-                cap = false;
+        if (num > 99999 || num < -99999)
+            System.out.println("Has introducido un número mayor de 5 dígitos");
+        else {
+            for (int i = 0; i < longNum / 2; i++) {
+                if (Integer.toString(num).charAt(i + neg) != Integer.toString(num).charAt(longNum - i - 1)) {
+                    cap = false;
+                }
             }
+            System.out.print((cap) ? num + " es capicúa" : num + " no es capicúa");
+            if (neg == 1)
+                System.out.print(" y de signo negativo");
         }
-        System.out.print((cap) ? num + " es capicúa" : num + " no es capicúa");
-        if (neg == 1)
-            System.out.print(" y de signo negativo");
+
         sc.close();
     }
 }
